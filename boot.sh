082369f0bf340c6c0d3e4b8f75f4c1ee6f21dcbc
Jeroen van 't Hert <jeroen@yottabyte.io>
2014-12-06 14:56:57 +0100

    Build Remmelts fork

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 
-go get github.com/docker/swarm && \
-go install github.com/docker/swarm && \
+go get github.com/remmelt/swarm && \
+go install github.com/remmelt/swarm && \
 cp $GOPATH/bin/swarm /tmp
